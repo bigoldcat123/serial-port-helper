17660b0e168512340f90ce2022652f3fe6d29b61
bigoldcat123 <a1456870251@163.com>
2025-06-09 17:28:00 +0800

    Refactor USB handling: Introduce UsbWriter and streamline main function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
 use nusb::{Interface, transfer::RequestBuffer};
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{Receiver, Sender};
+
+pub struct UsbWriter {
+    interface: Interface,
+    complete_reciver: Receiver<u8>,
+}
+impl UsbWriter {
+    pub fn new(interface: Interface, complete_reciver: Receiver<u8>) -> Self {
+        Self {
+            interface,
+            complete_reciver,
+        }
+    }
+    pub async fn write(&mut self, data: &[u8]) {
+        self.interface
+            .bulk_out(0x02, data.to_vec())
+            .await
+            .into_result()
+            .unwrap();
+        self.complete_reciver.recv().await.unwrap();
+    }
+}
 
 pub struct UsbReader {
     interface: Interface,
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::io::{Write, stdin, stdout};
 
 use tokio::sync::mpsc::channel;
-use usb_play::UsbReader;
-
+use usb_play::{UsbReader, UsbWriter};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let device = nusb::list_devices()
         .unwrap()
         .find(|x| x.vendor_id() == 0x1234)
-        .unwrap();
+        .expect("no such device");
     println!("{:#?}", device);
     let device = device.open().unwrap();
     println!("{:#?}", device.active_configuration());
@@ -18,21 +17,15 @@ async fn main() {
     let mut out = stdout();
     let in_ = stdin();
     let interface_in = interface.clone();
-    let (tx, mut rx) = channel(8);
+    let (tx, rx) = channel(8);
     let reader = UsbReader::new(interface_in, tx);
     tokio::spawn(async { reader.run().await });
+    let mut writer = UsbWriter::new(interface, rx);
     loop {
         let mut cmd = String::new();
-
         out.write_all(b">").unwrap();
         out.flush().unwrap();
         in_.read_line(&mut cmd).unwrap();
-        println!("{:?}",cmd.as_bytes());
-        interface
-            .bulk_out(0x02, cmd.into_bytes())
-            .await
-            .into_result()
-            .unwrap();
-        let _ = rx.recv().await.unwrap();
+        writer.write(cmd.as_bytes()).await;
     }
 }
