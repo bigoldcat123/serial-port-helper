b68c9352b082ac2199a6aafd1d406f57bfabb426
bigoldcat123 <a1456870251@163.com>
2025-06-27 19:32:22 +0800

    额

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::io::{Write, stdin, stdout};
+use std::{
+    fs::File,
+    io::{Read, Write, stdin, stdout},
+    time::Duration,
+};
 
-use tokio::sync::mpsc::channel;
+use tokio::{sync::mpsc::channel, time::sleep};
 use usb_play::{UsbReader, UsbWriter};
 
 #[tokio::main(flavor = "current_thread")]
@@ -14,7 +18,7 @@ async fn main() {
     println!("{:#?}", device.active_configuration());
     device.detach_and_claim_interface(0x01).unwrap();
     let interface = device.claim_interface(0x01).unwrap();
-    
+
     let mut out = stdout();
     let in_ = stdin();
     let interface_in = interface.clone();
@@ -24,11 +28,20 @@ async fn main() {
     tokio::spawn(async { reader.run().await });
 
     let mut writer = UsbWriter::new(interface, rx);
-    loop {
-        let mut cmd = String::new();
-        out.write_all(b">").unwrap();
-        out.flush().unwrap();
-        in_.read_line(&mut cmd).unwrap();
-        writer.write(cmd.as_bytes()).await;
+    // loop {
+    //     let mut cmd = String::new();
+    //     out.write_all(b">").unwrap();
+    //     out.flush().unwrap();
+    //     in_.read_line(&mut cmd).unwrap();
+    //     writer.write(cmd.as_bytes()).await;
+    // }
+    let mut f = File::open("/Users/dadigua/Desktop/embed-rust/huihui-150*100.bin").unwrap();
+    let mut buf = [0; 64];
+    while let Ok(n) = f.read(&mut buf) {
+        if n == 0 {
+            break;
+        }
+        writer.write(&buf[..n]).await;
+        // sleep(Duration::from_millis(2)).await;
     }
 }
